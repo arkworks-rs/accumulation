ee28da2d0724e66e6e0ac1a7feb34b3be1daee7c
William Lin <31808623+Will-Lin4@users.noreply.github.com>
2020-12-20 23:11:44 -0800

    Implement accumulation scheme based on PC_LH (#9)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //! A crate for accumulation schemes.
-#![deny(
+#![warn(
     const_err,
     future_incompatible,
     missing_docs,
@@ -29,13 +29,15 @@ extern crate std;
 
 #[cfg(not(feature = "std"))]
 #[macro_use]
-extern crate alloc as std;
+extern crate ark_std as std;
 
 /// Common data structures used by `AccumulationScheme` and `AidedAccumulationScheme`.
 pub mod data_structures;
 
 /// Common errors for `AccumulationScheme` and `AidedAccumulationScheme`.
 pub mod error;
+
+pub mod lh_as;
 
 /// An interface for an accumulation scheme. In an accumulation scheme for a predicate, a prover
 /// accumulates a stream of inputs into a single accumulator, which holds the necessary properties
@@ -203,6 +205,7 @@ pub mod tests {
 
                 let (accumulator, proof) =
                     A::prove(&pk, inputs, &old_accumulators, Some(&mut rng))?;
+
                 if !A::verify(
                     &vk,
                     Input::instances(inputs),
