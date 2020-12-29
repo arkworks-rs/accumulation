bb481a571b1bf9d05d1db5e8b7650688f02614a7
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-12-29 11:03:17 -0800

    Update for updates to poly-commit

--- src/lh_as/data_structures.rs
+++ src/lh_as/data_structures.rs
@@ -1,40 +1,38 @@
 use crate::std::vec::Vec;
-use ark_ff::{to_bytes, PrimeField, ToConstraintField};
-use ark_poly_commit::lh_pc::linear_hash::LinearHashFunction;
+use ark_ff::{to_bytes, ToConstraintField};
+use ark_ec::AffineCurve;
 use ark_poly_commit::{lh_pc, LabeledCommitment};
 use ark_sponge::Absorbable;
+use ark_poly_commit::lh_pc::{CommitterKey, Commitment};
 
-#[derive(Derivative)]
-#[derivative(Clone(bound = "F: PrimeField, LH: LinearHashFunction<F>"))]
-pub struct ProverKey<F: PrimeField, LH: LinearHashFunction<F>> {
-    pub(crate) lh_ck: lh_pc::CommitterKey<F, LH>,
-    pub(crate) degree_challenge: F,
+#[derive(Clone)]
+pub struct ProverKey<G: AffineCurve> {
+    pub(crate) lh_ck: CommitterKey<G>,
+    pub(crate) degree_challenge: G::ScalarField,
 }
 
-#[derive(Derivative)]
-#[derivative(Clone(bound = "F: PrimeField, LH: LinearHashFunction<F>"))]
-pub struct InputInstance<F: PrimeField, LH: LinearHashFunction<F>> {
-    pub commitment: LabeledCommitment<lh_pc::Commitment<F, LH>>,
-    pub point: F,
-    pub eval: F,
+#[derive(Clone)]
+pub struct InputInstance<G: AffineCurve> {
+    pub commitment: LabeledCommitment<Commitment<G>>,
+    pub point: G::ScalarField,
+    pub eval: G::ScalarField,
 }
 
-impl<F: PrimeField, LH: LinearHashFunction<F>> Absorbable<F> for InputInstance<F, LH> {
+impl<G: AffineCurve> Absorbable<G::ScalarField> for InputInstance<G> {
     fn to_sponge_bytes(&self) -> Vec<u8> {
         to_bytes!(&self.commitment, &self.point, &self.eval).unwrap()
     }
 
-    fn to_sponge_field_elements(&self) -> Vec<F> {
+    fn to_sponge_field_elements(&self) -> Vec<G::ScalarField> {
         self.to_sponge_bytes().to_field_elements().unwrap()
     }
 }
 
-#[derive(Derivative)]
-#[derivative(Clone(bound = "F: PrimeField, LH: LinearHashFunction<F>"))]
-pub struct SingleProof<F: PrimeField, LH: LinearHashFunction<F>> {
-    pub(crate) witness_commitment: LabeledCommitment<lh_pc::Commitment<F, LH>>,
-    pub(crate) witness_eval: F,
-    pub(crate) eval: F,
+#[derive(Clone)]
+pub struct SingleProof<G: AffineCurve> {
+    pub(crate) witness_commitment: LabeledCommitment<lh_pc::Commitment<G>>,
+    pub(crate) witness_eval: G::ScalarField,
+    pub(crate) eval: G::ScalarField,
 }
 
-pub type Proof<F, LH> = Vec<SingleProof<F, LH>>;
+pub type Proof<G> = Vec<SingleProof<G>>;
